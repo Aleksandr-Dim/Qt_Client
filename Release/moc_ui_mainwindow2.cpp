4507b4ee39529f6c14164ef12597cc4d5b547920
Aleksandr-Dim <98106196+Aleksandr-Dim@users.noreply.github.com>
2022-01-20 18:05:03 +0300

    Add files via upload

---
+++ Release/moc_ui_mainwindow2.cpp
@@ -0,0 +1,436 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ui_mainwindow2.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../ui_mainwindow2.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ui_mainwindow2.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Server_t {
+    const uint offsetsAndSize[6];
+    char stringdata0[22];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_Server_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_Server_t qt_meta_stringdata_Server = {
+    {
+QT_MOC_LITERAL(0, 6), // "Server"
+QT_MOC_LITERAL(7, 13), // "newConnection"
+QT_MOC_LITERAL(21, 0) // ""
+
+    },
+    "Server\0newConnection\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Server[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   20,    2, 0x0a,    1 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Server::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Server *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->newConnection(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject Server::staticMetaObject = { {
+    QMetaObject::SuperData::link<QThread::staticMetaObject>(),
+    qt_meta_stringdata_Server.offsetsAndSize,
+    qt_meta_data_Server,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_Server_t
+, QtPrivate::TypeAndForceComplete<Server, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *Server::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Server::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Server.stringdata0))
+        return static_cast<void*>(this);
+    return QThread::qt_metacast(_clname);
+}
+
+int Server::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 1;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_ServerThread_t {
+    const uint offsetsAndSize[8];
+    char stringdata0[25];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_ServerThread_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_ServerThread_t qt_meta_stringdata_ServerThread = {
+    {
+QT_MOC_LITERAL(0, 12), // "ServerThread"
+QT_MOC_LITERAL(13, 4), // "read"
+QT_MOC_LITERAL(18, 0), // ""
+QT_MOC_LITERAL(19, 5) // "close"
+
+    },
+    "ServerThread\0read\0\0close"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ServerThread[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x0a,    1 /* Public */,
+       3,    0,   27,    2, 0x0a,    2 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ServerThread::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ServerThread *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->read(); break;
+        case 1: _t->close(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject ServerThread::staticMetaObject = { {
+    QMetaObject::SuperData::link<QThread::staticMetaObject>(),
+    qt_meta_stringdata_ServerThread.offsetsAndSize,
+    qt_meta_data_ServerThread,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_ServerThread_t
+, QtPrivate::TypeAndForceComplete<ServerThread, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *ServerThread::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ServerThread::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ServerThread.stringdata0))
+        return static_cast<void*>(this);
+    return QThread::qt_metacast(_clname);
+}
+
+int ServerThread::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_DownloadFileThread_t {
+    const uint offsetsAndSize[6];
+    char stringdata0[25];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_DownloadFileThread_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_DownloadFileThread_t qt_meta_stringdata_DownloadFileThread = {
+    {
+QT_MOC_LITERAL(0, 18), // "DownloadFileThread"
+QT_MOC_LITERAL(19, 4), // "read"
+QT_MOC_LITERAL(24, 0) // ""
+
+    },
+    "DownloadFileThread\0read\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DownloadFileThread[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   20,    2, 0x0a,    1 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void DownloadFileThread::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<DownloadFileThread *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->read(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject DownloadFileThread::staticMetaObject = { {
+    QMetaObject::SuperData::link<QThread::staticMetaObject>(),
+    qt_meta_stringdata_DownloadFileThread.offsetsAndSize,
+    qt_meta_data_DownloadFileThread,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_DownloadFileThread_t
+, QtPrivate::TypeAndForceComplete<DownloadFileThread, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *DownloadFileThread::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DownloadFileThread::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_DownloadFileThread.stringdata0))
+        return static_cast<void*>(this);
+    return QThread::qt_metacast(_clname);
+}
+
+int DownloadFileThread::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QThread::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 1;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_Window_t {
+    const uint offsetsAndSize[14];
+    char stringdata0[79];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_Window_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_Window_t qt_meta_stringdata_Window = {
+    {
+QT_MOC_LITERAL(0, 6), // "Window"
+QT_MOC_LITERAL(7, 14), // "LoadDirPressed"
+QT_MOC_LITERAL(22, 0), // ""
+QT_MOC_LITERAL(23, 11), // "LoadPressed"
+QT_MOC_LITERAL(35, 18), // "DownloadDirPressed"
+QT_MOC_LITERAL(54, 15), // "DownloadPressed"
+QT_MOC_LITERAL(70, 8) // "GetTable"
+
+    },
+    "Window\0LoadDirPressed\0\0LoadPressed\0"
+    "DownloadDirPressed\0DownloadPressed\0"
+    "GetTable"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Window[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   44,    2, 0x0a,    1 /* Public */,
+       3,    0,   45,    2, 0x0a,    2 /* Public */,
+       4,    0,   46,    2, 0x0a,    3 /* Public */,
+       5,    0,   47,    2, 0x0a,    4 /* Public */,
+       6,    0,   48,    2, 0x0a,    5 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Window::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Window *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->LoadDirPressed(); break;
+        case 1: _t->LoadPressed(); break;
+        case 2: _t->DownloadDirPressed(); break;
+        case 3: _t->DownloadPressed(); break;
+        case 4: _t->GetTable(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject Window::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Window.offsetsAndSize,
+    qt_meta_data_Window,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_Window_t
+, QtPrivate::TypeAndForceComplete<Window, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *Window::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Window::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Window.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Window::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 5;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
